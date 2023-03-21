e94313cc6e789c6bd3108b26736f6baf757ff11e
nikgeeck <v.nik3000@gmail.com>
2023-03-21 22:07:05 +0300

    Коммит данных, тк теперь они добавляются в конфиге

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
-insert into Ingredients (id, name, type)
-values ('GRBF', 'Ground Beef', 'PROTEIN');
-insert into Ingredients (id, name, type)
-values ('CARN', 'Carnitas', 'PROTEIN');
-insert into Ingredients (id, name, type)
-values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
-insert into Ingredients (id, name, type)
-values ('LETC', 'Lettuce', 'VEGGIES');
-insert into Ingredients (id, name, type)
-values ('CHED', 'Cheddar', 'CHEESE');
-insert into Ingredients (id, name, type)
-values ('JACK', 'Monterrey Jack', 'CHEESE');
-insert into Ingredients (id, name, type)
-values ('SLSA', 'Salsa', 'SAUCE');
-insert into Ingredients (id, name, type)
-values ('SRCR', 'Sour Cream', 'SAUCE');
+-- insert into Ingredients (id, name, type)
+-- values ('GRBF', 'Ground Beef', 'PROTEIN');
+-- insert into Ingredients (id, name, type)
+-- values ('CARN', 'Carnitas', 'PROTEIN');
+-- insert into Ingredients (id, name, type)
+-- values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
+-- insert into Ingredients (id, name, type)
+-- values ('LETC', 'Lettuce', 'VEGGIES');
+-- insert into Ingredients (id, name, type)
+-- values ('CHED', 'Cheddar', 'CHEESE');
+-- insert into Ingredients (id, name, type)
+-- values ('JACK', 'Monterrey Jack', 'CHEESE');
+-- insert into Ingredients (id, name, type)
+-- values ('SLSA', 'Salsa', 'SAUCE');
+-- insert into Ingredients (id, name, type)
+-- values ('SRCR', 'Sour Cream', 'SAUCE');
