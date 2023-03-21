28b4b7d7e69736567325f5696d57abc94282718c
nikgeeck <v.nik3000@gmail.com>
2023-03-21 22:10:41 +0300

    Добавление spring data в репозитории классов OrderRepository и IngredientRepository при помощи наследования от CRUD репозитория

--- src/main/java/com/repository/OrderRepository.java
+++ src/main/java/com/repository/OrderRepository.java
@@ -1,9 +1,10 @@
 package com.repository;
 
 import com.model.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     TacoOrder save(TacoOrder order);
 }
