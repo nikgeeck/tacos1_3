a0a71039885cbeaafaeb7bdaf63609d7b87bb2a6
nikgeeck <v.nik3000@gmail.com>
2023-04-23 23:02:14 +0300

    Убрал лишние импорты

--- src/main/java/com/repository/IngredientRepository.java
+++ src/main/java/com/repository/IngredientRepository.java
@@ -1,12 +1,9 @@
 package com.repository;
 
 import com.model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.Optional;
-//import org.springframework.data.repository.CrudRepository;
 
 public interface IngredientRepository extends ListCrudRepository<Ingredient, String> {
 //    Iterable<Ingredient> findAll();
